67663b8728506fe87f555fe4addc4322f8bdede1
Danil <66364660+eXseM@users.noreply.github.com>
2020-12-10 16:29:40 +0300

    Early version

---
+++ src/components/Catalog.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Catalog</h1>
+        <catalog-item />
+    </div>
+</template>
+
+<script>
+import CatalogItem from '@/components/CatalogItem'
+
+    export default {
+        name: 'Catalog',
+        components:{
+            CatalogItem
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="main">
+        <h1>Main</h1>
+        <Catalog />
+    </div>
+</template>
+
+<script>
+import Catalog from '@/components/Catalog'
+
+    export default {
+        name: 'Main',
+        components:{
+            Catalog
+        },
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.main{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+}
+</style>
